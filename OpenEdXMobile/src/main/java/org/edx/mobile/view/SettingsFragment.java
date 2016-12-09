e989ffa2cb1642ead752c6fd6ef115dc5732d47d
Albert St. Aubin <astaubin@edx.org>
2016-12-09 06:46:22 -0500

    Updated setting to only be shown when removable storage is available.

--- OpenEdXMobile/src/main/java/org/edx/mobile/view/SettingsFragment.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/SettingsFragment.java
@@ -1,6 +1,9 @@
 package org.edx.mobile.view;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +12,7 @@ import android.widget.LinearLayout;
 import android.widget.Switch;
 
 import com.google.inject.Inject;
+import com.google.repacked.antlr.v4.runtime.misc.NotNull;
 
 import org.edx.mobile.R;
 import org.edx.mobile.base.BaseFragment;
@@ -18,6 +22,8 @@ import org.edx.mobile.module.analytics.ISegment;
 import org.edx.mobile.module.prefs.PrefManager;
 import org.edx.mobile.view.dialog.IDialogCallback;
 import org.edx.mobile.view.dialog.NetworkCheckDialogFragment;
+
+import java.io.File;
 
 public class SettingsFragment extends BaseFragment {
 
@@ -33,6 +39,7 @@ public class SettingsFragment extends BaseFragment {
 
     private Switch wifiSwitch;
     private Switch mSDCardSwitch;
+    private LinearLayout mSDCardSetting;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,6 +54,7 @@ public class SettingsFragment extends BaseFragment {
         final View layout = inflater.inflate(R.layout.fragment_settings, container, false);
         wifiSwitch = (Switch) layout.findViewById(R.id.wifi_setting);
         mSDCardSwitch = (Switch) layout.findViewById(R.id.download_location_switch);
+        mSDCardSetting = (LinearLayout) layout.findViewById(R.id.sd_card_setting);
 
         updateWifiSwitch();
         updateSDCardSwitch();
@@ -77,17 +85,34 @@ public class SettingsFragment extends BaseFragment {
         });
     }
 
-    private void updateSDCardSwitch(){
-        final PrefManager prefManager =
-                new PrefManager(getActivity().getBaseContext(), PrefManager.Pref.SD_CARD);
-        mSDCardSwitch.setOnCheckedChangeListener(null);
-        mSDCardSwitch.setChecked(prefManager.getBoolean(PrefManager.Key.DOWNLOAD_TO_SDCARD, true));
-        mSDCardSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                prefManager.put(PrefManager.Key.DOWNLOAD_TO_SDCARD, isChecked);
+    private boolean isRemovableStorageAvailable(@NotNull Context context){
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= Build.VERSION_CODES.LOLLIPOP) {
+            File[] fileList = context.getExternalFilesDirs("Android");
+            for (File extFile : fileList){
+                if (extFile != null && Environment.isExternalStorageRemovable(extFile)){
+                    return true;
+                }
             }
-        });
+        }
+        return false;
+    }
+
+    private void updateSDCardSwitch(){
+        if (this.isRemovableStorageAvailable(getContext())) {
+            final PrefManager prefManager =
+                    new PrefManager(getActivity().getBaseContext(), PrefManager.Pref.SD_CARD);
+            mSDCardSwitch.setOnCheckedChangeListener(null);
+            mSDCardSwitch.setChecked(prefManager.getBoolean(PrefManager.Key.DOWNLOAD_TO_SDCARD, true));
+            mSDCardSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                    prefManager.put(PrefManager.Key.DOWNLOAD_TO_SDCARD, isChecked);
+                }
+            });
+        } else {
+            mSDCardSetting.setVisibility(View.INVISIBLE);
+        }
 
     }
 
